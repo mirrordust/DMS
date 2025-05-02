724935401e45d0a4d2670d4d8235cee32798d56c
mirrordust <gsmr19re@outlook.com>
2025-05-02 20:48:38 +0800

    install fonts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import './assets/main.css'
 
+import '@fontsource/roboto-condensed'
+import '@fontsource/rem' // Defaults to weight 400
+import '@fontsource/poppins' // Defaults to weight 400
+import '@fontsource/poppins/400.css'
+import '@fontsource/inter' // Defaults to weight 400
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
